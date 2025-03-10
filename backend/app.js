11c368151319bb9e81410f1135369c0402ccce5e
Imeen01 <hajejiimeen@gmail.com>
2025-03-10 01:58:28 +0100

    Création des dossiers database et models

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
@@ -11,4 +11,8 @@ app.use(cors());
 app.get("/", (req, res) => res.send("API en marche"));
 
 const PORT = process.env.PORT || 5000;
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Error connecting to MongoDB:', err));
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
